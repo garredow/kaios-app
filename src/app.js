d2ba58d5e65a99cfd2c2639d1936bed070019f2c
Garrett Downs <garredow@outlook.com>
2022-04-02 12:07:22 -0400

    test

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 console.log('hello');
 
-async function go() {
+function go() {
   const bundleId = location.search.split('=')[1];
   console.log('bundleId', bundleId);
   if (!bundleId) return;
 
-  const app = await fetch(`/.netlify/functions/get-app?bundleId=${bundleId}`).then((res) =>
-    res.json()
-  );
-  console.log('app', app);
-
-  new MozActivity({
-    name: 'open-app',
-    data: {
-      url: app.manifest_url,
-      type: 'url',
-      appName: app.name,
-    },
-  });
+  const app = fetch(`/.netlify/functions/get-app?bundleId=${bundleId}`)
+    .then((res) => res.json())
+    .then((app) => {
+      console.log('app', app);
+      new MozActivity({
+        name: 'open-app',
+        data: {
+          url: app.manifest_url,
+          type: 'url',
+          appName: app.name,
+        },
+      });
+    });
 }
 
 document.querySelector('#btnOpenStore').addEventListener('click', go);
